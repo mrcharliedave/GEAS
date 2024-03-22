eebe78648d8f797647726405e8350885de708435
Charlie Dave <charliedavidmaxwell@gmail.com>
2024-03-22 15:30:42 +0000

    [+] Added new base class for actors. [^] Updated dependencies.

---
+++ Source/GEAS/Actors/G2DActor.cpp
@@ -0,0 +1,8 @@
+﻿#include "G2DActor.h"
+
+
+AG2DActor::AG2DActor(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.DoNotCreateDefaultSubobject(ACharacter::MeshComponentName))
+{
+	// Lol
+}
---
+++ Source/GEAS/Actors/G2DActor.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include "GameplayTagContainer.h"
+#include "PaperCharacter.h"
+#include "G2DActor.generated.h"
+
+UCLASS()
+class GEAS_API AG2DActor : public APaperCharacter
+{
+	GENERATED_UCLASS_BODY()
+	UFUNCTION()
+	void CreateCharacter();
+public:
+
+private:
+	FGameplayTag mCharacterBaseType;
+};
