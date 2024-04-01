3abaf25b3e31f90f0b71af8757d6a0c412ca631e
Charlie Dave <charliedavidmaxwell@gmail.com>
2024-04-01 23:45:39 +0100

    [+] Added base game classes.

---
+++ Source/GEAS/GEASGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GEASGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEAS_API UGEASGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
